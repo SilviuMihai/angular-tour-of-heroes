e64e34d393a7546568e6e47e5abf4ddd76f992c2
SilviuMihai <silviu96@gmail.com>
2020-10-13 08:56:24 +0300

    Using Observable to return the list of HEROES, because now the implementation is asynchronous. (simulating a server)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -28,7 +28,19 @@ export class HeroesComponent implements OnInit {
 
   getHeroes()
   {
-    this.heroes = this.heroService.getHeroes();
-    //returns the list of HEROES from the SERVICE
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
+    //getHeroes() method returns the list of HEROES and subscribe just fill's up the parameter this.heroes.
   }
+//The details behind the Observable
+/* The previous version assigns an array of heroes to the component's heroes property. 
+The assignment occurs synchronously, as if the server could return heroes instantly or the browser could 
+freeze the UI while it waited for the server's response.
+
+That won't work when the HeroService is actually making requests of a remote server.
+
+The new version waits for the Observable to emit the array of heroes—which could happen now or several 
+minutes from now. The subscribe() method passes the emitted array to the callback, which sets the component's 
+heroes property.
+
+This asynchronous approach will work when the HeroService requests heroes from the server. */
 }
