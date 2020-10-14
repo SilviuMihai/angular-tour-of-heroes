a53624c37520aa504170ecf11bf3faafd3a64c16
SilviuMihai <silviu96@gmail.com>
2020-10-14 08:36:37 +0300

    Created dashboard component, also used routerLink to go to different pages in the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HeroesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+/* A typical Angular Route has two properties:
+path: a string that matches the URL in the browser address bar.
+component: the component that the router should create when navigating to this route. */
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  heroes:Hero[];
+
+  constructor(private heroService:HeroService) {
+   }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes()
+  {
+    this.heroService.getHeroes().subscribe(hero=>this.heroes=hero.slice(1,5));
+  }
+}
