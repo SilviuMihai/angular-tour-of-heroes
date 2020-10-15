40fb2a0cd5270342b5171a49178e47e323661e20
SilviuMihai <silviu96@gmail.com>
2020-10-15 09:53:23 +0300

    Delete a Hero using DELETE METHOD

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -26,4 +26,26 @@ export class HeroesComponent implements OnInit {
   {
     this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
   }
+
+  add(name:string)
+  {
+    name = name.trim();
+    if(!name)
+    { return; }
+    this.heroService.addHero({ name } as Hero).subscribe(hero => { this.heroes.push(hero)});
+  }
+
+  delete(hero:Hero)
+  {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
+
+/* There's really nothing for the component to do with the Observable returned by heroService.delete() 
+but it must subscribe anyway.
+
+If you neglect to subscribe(), the service will not send the delete request to the server. 
+As a rule, an Observable does nothing until something subscribes.
+Confirm this for yourself by temporarily removing the subscribe(), clicking "Dashboard", 
+then clicking "Heroes". You'll see the full list of heroes again. */
