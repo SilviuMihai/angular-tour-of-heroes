7487fe424c02c07cec05d394441d22f7c5d6de70
SilviuMihai <silviu96@gmail.com>
2020-10-15 08:43:48 +0300

    Used GET METHOD to retrieve the heroes from database(memory in our case)

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,27 +3,38 @@ import { Hero } from './../hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';
 
+  constructor(private http: HttpClient,private messageService: MessageService) { }
 
-  constructor(private messageService: MessageService) { }
+  
 
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]>
   {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes')
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id:number): Observable<Hero>
   {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find( hero=> hero.id===id ) );
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message:string)
+  {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
 /* Note the backticks ( ` ) that define a JavaScript template literal for embedding the id. */
